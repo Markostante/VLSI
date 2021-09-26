c6ab3141a9ba326a2b0b6ad74b05ad733e6a7685
Markostante <markostante97@gmail.com>
2021-09-26 16:35:10 +0200

    Add files via upload

---
+++ utilities/instances_to_dzn.py
@@ -0,0 +1,21 @@
+def read_instance_text(f):
+    print(f)
+    file = open(f)
+    width = int(file.readline())
+    n_pieces = int(file.readline())
+    DX = []
+    DY = []
+    for i in range(n_pieces):
+        piece = file.readline()
+        split_piece = piece.strip().split(" ")
+        DX.append(int(split_piece[0]))
+        DY.append(int(split_piece[1]))
+    return width, n_pieces, DX, DY
+
+def write_dzn(width, n_pieces, DX, DY, out_path="./file.dzn"):
+    file = open(out_path, mode="w")
+    file.write(f"width = {width};\n")
+    file.write(f"n = {n_pieces};\n")
+    file.write(f"DX = {DX};\n")
+    file.write(f"DY = {DY};")
+    file.close()
